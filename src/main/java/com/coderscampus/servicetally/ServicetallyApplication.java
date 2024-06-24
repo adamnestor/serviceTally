f6e5401bbccb4f4797770c298737385685eb2579
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-06-24 11:40:30 -0400

    init

---
+++ src/main/java/com/coderscampus/servicetally/ServicetallyApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.servicetally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicetallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicetallyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/servicetally/ServicetallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.servicetally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicetallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
