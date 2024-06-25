6ab16e95224129a49bf3b54c7eea51a54a09b846
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-06-25 16:51:04 -0400

    Created User and UsersType Repositories

---
+++ src/main/java/com/coderscampus/servicetally/repository/UsersTypeRepository.java
@@ -0,0 +1,9 @@
+package com.coderscampus.servicetally.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.coderscampus.servicetally.domain.UsersType;
+
+public interface UsersTypeRepository extends JpaRepository<UsersType, Integer> {
+
+}
