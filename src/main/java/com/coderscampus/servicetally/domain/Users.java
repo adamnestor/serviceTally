633089431f7ae61aafa55a4f9b5304caf2abdb2f
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-06-25 15:38:16 -0400

    Created Users and UsersType Entities. Also updated Application.properties to talk with DB

---
+++ src/main/java/com/coderscampus/servicetally/domain/Users.java
@@ -0,0 +1,80 @@
+package com.coderscampus.servicetally.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userId;
+
+	@Column(unique = true)
+	private String email;
+
+	@NotEmpty
+	private String password;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "userTypeId", referencedColumnName = "userTypeId")
+	private UsersType userTypeId;
+
+	public Users() {
+	}
+
+	public Users(int userId, String email, @NotEmpty String password, UsersType userTypeId) {
+		this.userId = userId;
+		this.email = email;
+		this.password = password;
+		this.userTypeId = userTypeId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UsersType getUserTypeId() {
+		return userTypeId;
+	}
+
+	public void setUserTypeId(UsersType userTypeId) {
+		this.userTypeId = userTypeId;
+	}
+
+	@Override
+	public String toString() {
+		return "Users [userId=" + userId + ", email=" + email + ", password=" + password + ", userTypeId=" + userTypeId
+				+ "]";
+	}
+
+}
