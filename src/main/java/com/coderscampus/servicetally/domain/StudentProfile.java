b7e8e982431dc6728ed3ccbfb63627f51ec41aba
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-06-26 13:06:24 -0400

    Create StudentProfile.java

---
+++ src/main/java/com/coderscampus/servicetally/domain/StudentProfile.java
@@ -0,0 +1,11 @@
+package com.coderscampus.servicetally.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "student_profile")
+public class StudentProfile {
+	
+	
+}
