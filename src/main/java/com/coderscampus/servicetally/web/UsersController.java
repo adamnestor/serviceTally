d1f3c675a2652befcda0d4730e3421c91218da3d
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-06-26 11:25:19 -0400

    Added PostMapping for registering a new user to the db

--- src/main/java/com/coderscampus/servicetally/web/UsersController.java
+++ src/main/java/com/coderscampus/servicetally/web/UsersController.java
@@ -6,22 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.coderscampus.servicetally.domain.Users;
 import com.coderscampus.servicetally.domain.UsersType;
 import com.coderscampus.servicetally.service.UsersTypeService;
 
+import jakarta.validation.Valid;
+
 @Controller
 public class UsersController {
 
-	
 	private final UsersTypeService usersTypeService;
 
 	@Autowired
 	public UsersController(UsersTypeService usersTypeService) {
 		this.usersTypeService = usersTypeService;
 	}
-	
+
 	// Show user registration form
 	@GetMapping("/register")
 	public String register(Model model) {
@@ -30,5 +32,12 @@ public class UsersController {
 		model.addAttribute("user", new Users());
 		return "register";
 	}
-	
+
+	// Create user and send to db
+	@PostMapping("/register/new")
+	public String userRegistration(@Valid Users users) {
+		System.out.println("User:: " + users);
+		return "dashboard";
+	}
+
 }
