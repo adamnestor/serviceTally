5bd004686e679d900a81272b0b15850b73961dc2
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-07-02 14:42:20 -0400

    update school with annotations

--- src/main/java/com/coderscampus/servicetally/domain/School.java
+++ src/main/java/com/coderscampus/servicetally/domain/School.java
@@ -1,5 +1,10 @@
 package com.coderscampus.servicetally.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "school")
 public class School {
 
 }
