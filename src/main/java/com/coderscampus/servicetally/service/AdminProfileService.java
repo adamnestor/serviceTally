7ffdb07e48a43b22f02f49f0b657b726d4393acd
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-07-09 15:08:20 -0400

    Put admin user on model

--- src/main/java/com/coderscampus/servicetally/service/AdminProfileService.java
+++ src/main/java/com/coderscampus/servicetally/service/AdminProfileService.java
@@ -4,6 +4,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.coderscampus.servicetally.domain.AdminProfile;
 import com.coderscampus.servicetally.repository.AdminProfileRepository;
@@ -21,7 +24,6 @@ public class AdminProfileService {
 	public Optional<AdminProfile> getOne(Integer id){
 		return adminProfileRepo.findById(id);
 	}
-	
-	
+
 
 }
--- src/main/java/com/coderscampus/servicetally/web/AdminProfileController.java
+++ src/main/java/com/coderscampus/servicetally/web/AdminProfileController.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.coderscampus.servicetally.domain.AdminProfile;
 import com.coderscampus.servicetally.domain.Users;
@@ -32,20 +34,36 @@ public class AdminProfileController {
 
 	@GetMapping("/")
 	public String adminProfile(Model model) {
-		
+
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		
+
 		if (!(authentication instanceof AnonymousAuthenticationToken)) {
 			String currentUsername = authentication.getName();
-			Users user = usersRepo.findByEmail(currentUsername).orElseThrow(()-> new UsernameNotFoundException("Could not find user"));
+			Users user = usersRepo.findByEmail(currentUsername)
+					.orElseThrow(() -> new UsernameNotFoundException("Could not find user"));
 			Optional<AdminProfile> adminProfile = adminProfileService.getOne(user.getUserId());
-			
-			if(!adminProfile.isEmpty())
+
+			if (!adminProfile.isEmpty())
 				model.addAttribute("profile", adminProfile.get());
-			
+
 		}
-		
+
 		return "admin_profile";
+	}
+
+	public String addNew(AdminProfile adminProfile, @RequestParam("image") MultipartFile multipartFile, Model model) {
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (!(authentication instanceof AnonymousAuthenticationToken)) {
+			String currentUsername = authentication.getName();
+			Users user = usersRepo.findByEmail(currentUsername)
+					.orElseThrow(() -> new UsernameNotFoundException("Could not find user"));
+			adminProfile.setUserId(user);
+			adminProfile.setUserAccountId(user.getUserId());
+		}
+		model.addAttribute("profile", adminProfile);
+
+		return "redirect:/dashboard/";
 	}
 
 }
