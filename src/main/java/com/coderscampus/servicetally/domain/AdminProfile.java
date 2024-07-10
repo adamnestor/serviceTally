2dbcf072972f505412f22e58646cc0b8f4479712
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-07-10 16:09:41 -0400

    Update AdminProfile.java

--- src/main/java/com/coderscampus/servicetally/domain/AdminProfile.java
+++ src/main/java/com/coderscampus/servicetally/domain/AdminProfile.java
@@ -3,8 +3,6 @@ package com.coderscampus.servicetally.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.luv2code.jobportal.entity.Users;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
