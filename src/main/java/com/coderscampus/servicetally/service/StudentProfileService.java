1d0e56e53b98eeb19d704dfa3d25b806812d3dac
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-07-12 10:32:09 -0400

    Added getOne and addNew methods to StudentProfileService

--- src/main/java/com/coderscampus/servicetally/service/StudentProfileService.java
+++ src/main/java/com/coderscampus/servicetally/service/StudentProfileService.java
@@ -2,13 +2,30 @@ package com.coderscampus.servicetally.service;
 
 import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
 import com.coderscampus.servicetally.domain.StudentProfile;
 import com.coderscampus.servicetally.repository.StudentProfileRepository;
 
+@Service
 public class StudentProfileService {
+	
+	private final StudentProfileRepository studentProfileRepo;
+
+	public StudentProfileService(StudentProfileRepository studentProfileRepo) {
+		this.studentProfileRepo = studentProfileRepo;
+	}
+
+
 
 	public Optional<StudentProfile> getOne(Integer id) {
-		return StudentProfileRepository.findById(id);
+		return studentProfileRepo.findById(id);
 	}
+
+	public StudentProfile addNew(StudentProfile studentProfile) {
+		return studentProfileRepo.save(studentProfile);
+	}
+	
+	
 
 }
