590dc3b48f64b54209f58818336ac6fe2581a5d8
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2025-02-15 22:28:26 -0500

    Update StudentProfileRepository.java

--- src/main/java/com/coderscampus/servicetally/repository/StudentProfileRepository.java
+++ src/main/java/com/coderscampus/servicetally/repository/StudentProfileRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.coderscampus.servicetally.domain.GraduationYearDto;
 import com.coderscampus.servicetally.domain.School;
 import com.coderscampus.servicetally.domain.StudentProfile;
 
